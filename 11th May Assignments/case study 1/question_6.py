f73caa1cb54b9e29475bbc1a8c4966dffb935a9c
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-05-08 21:33:24 +0530

    question 6

--- 11th May Assignments/case study 1/question_6.py
+++ 11th May Assignments/case study 1/question_6.py
@@ -1,2 +1,9 @@
-# 6. Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). 
+# 6. Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+# between 2000 and 3200 (both included).
 # The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+
+result_list = [str(number) for number in range(
+    2000, 3201) if (not number % 7) and number % 5]
+
+print(",".join(result_list))
