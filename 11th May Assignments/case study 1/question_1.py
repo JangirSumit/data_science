cdcb6f8202e71f762aadc4b8db41e2abc249d1bd
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-05-08 22:09:29 +0530

    question 1 and 4

--- 11th May Assignments/case study 1/question_1.py
+++ 11th May Assignments/case study 1/question_1.py
@@ -1,8 +1,44 @@
-# 1. A Robot moves in a Plane starting from the origin point (0,0). The robot can move toward UP, DOWN, LEFT, RIGHT. 
+# 1. A Robot moves in a Plane starting from the origin point (0,0). The robot can move toward UP, DOWN, LEFT, RIGHT.
 # The trace of Robot movement is as given following:
 # UP 5
 # DOWN 3
 # LEFT 3
 # RIGHT 2
-# The numbers after directions are steps.  
-# Write a program to compute the distance current position after sequence of movements.Hint: Use math module.
+# The numbers after directions are steps.
+# Write a program to compute the distance current position after sequence of movements.
+# Hint: Use math module.
+
+import math
+
+X = 0
+Y = 0
+
+
+def get_coordinates(string):
+    global X
+    global Y
+    if string[0] == "UP":
+        Y += int(string[1])
+    elif string[0] == "DOWN":
+        Y -= int(string[1])
+    elif string[0] == "LEFT":
+        X -= int(string[1])
+    elif string[0] == "RIGHT":
+        X += int(string[1])
+
+
+list_moves = []
+
+print("Enter ROBOT's movement")
+while True:
+    move = input("[Enter 1 to end]>>> ")
+    list_moves.append(move)
+
+    if move == "1":
+        for move in list_moves:
+            get_coordinates(move.split(" "))
+
+        distance_from_origin = math.sqrt((X**2)+(Y**2))
+        print(f"Distance from Origin : {distance_from_origin}")
+        print(f"Final position : ({X}, {Y})")
+        break
--- 11th May Assignments/case study 1/question_4.py
+++ 11th May Assignments/case study 1/question_4.py
@@ -1,2 +1,2 @@
-# 4. Write a program to find distancebetween two locations when their latitude and longitudes are given.
+# 4. Write a program to find distance between two locations when their latitude and longitudes are given.
 # Hint: Use math module.
