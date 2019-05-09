a1ce58334ce5abdcebd5906a05c3c97322b3b4f4
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-05-09 11:00:17 +0530

    question 2 and 3

--- 11th May Assignments/case study 1/question_3.py
+++ 11th May Assignments/case study 1/question_3.py
@@ -1,2 +1,17 @@
 # 3. Weather forecasting organization wants to show is it day or night. 
-# So, write a program for such organization to findwhether is it dark outside or not.Hint: Use time module.
+# So, write a program for such organization to find whether is it dark outside or not.
+# Hint: Use time module.
+
+from datetime import datetime
+
+def get_part_of_day(hour):
+    return (
+        "light" if 5 <= hour <= 18
+        else
+        "dark" 
+    )
+
+h = datetime.now().hour
+
+print(f"Its {get_part_of_day(h)}  right now.")
+
