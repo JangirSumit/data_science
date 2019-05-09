b7c840cde38f79d2a684c0bb8dcaa2d87c8c2af3
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-05-09 19:21:36 +0530

    indentation changes

--- 11th May Assignments/case study 1/question_15.py
+++ 11th May Assignments/case study 1/question_15.py
@@ -3,21 +3,19 @@
 import math
 
 # list
-print(sum([1,2,3,4,5,6,7,8,9]))
+print(sum([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 # tuple
-print(sum((1,2,3,4,5,6,7,8,9)))
+print(sum((1, 2, 3, 4, 5, 6, 7, 8, 9)))
 # set
-print(sum({1,2,3,4,5,6,7,8,9}))
+print(sum({1, 2, 3, 4, 5, 6, 7, 8, 9}))
 # range
-print(sum(range(1,10)))
+print(sum(range(1, 10)))
 
 # list
-print(math.fsum([1,2,3,4,5,6,7,8,9]))
+print(math.fsum([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 # tuple
-print(math.fsum((1,2,3,4,5,6,7,8,9)))
+print(math.fsum((1, 2, 3, 4, 5, 6, 7, 8, 9)))
 # set
-print(math.fsum({1,2,3,4,5,6,7,8,9}))
+print(math.fsum({1, 2, 3, 4, 5, 6, 7, 8, 9}))
 # range
-print(math.fsum(range(1,10)))
-
-
+print(math.fsum(range(1, 10)))
