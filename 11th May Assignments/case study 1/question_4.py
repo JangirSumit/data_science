f446df7f4f26de50c0ec243b821235d48c47c680
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-05-09 11:40:45 +0530

    question 4

--- 11th May Assignments/case study 1/question_4.py
+++ 11th May Assignments/case study 1/question_4.py
@@ -1,2 +1,6 @@
 # 4. Write a program to find distance between two locations when their latitude and longitudes are given.
 # Hint: Use math module.
+
+# https://www.movable-type.co.uk/scripts/latlong.html
+
+#
