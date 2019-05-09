cc7cde44dfad3a89548f8a2797f7ed288cde798d
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-05-09 11:17:50 +0530

    question 15

--- 11th May Assignments/case study 1/question_15.py
+++ 11th May Assignments/case study 1/question_15.py
@@ -1,1 +1,23 @@
 # 15. Give example of fsum and sum function of math library
+
+import math
+
+# list
+print(sum([1,2,3,4,5,6,7,8,9]))
+# tuple
+print(sum((1,2,3,4,5,6,7,8,9)))
+# set
+print(sum({1,2,3,4,5,6,7,8,9}))
+# range
+print(sum(range(1,10)))
+
+# list
+print(math.fsum([1,2,3,4,5,6,7,8,9]))
+# tuple
+print(math.fsum((1,2,3,4,5,6,7,8,9)))
+# set
+print(math.fsum({1,2,3,4,5,6,7,8,9}))
+# range
+print(math.fsum(range(1,10)))
+
+
