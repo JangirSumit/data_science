bdcd8baed4f06e5fa1e3f9d08e7cd56cdef0997d
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-05-24 09:58:02 +0530

    assignment 2

--- 17th May Assignments/case study 1/question_2.py
+++ 17th May Assignments/case study 1/question_2.py
@@ -1,21 +1,22 @@
 # 2. Find the genre in which there has been the greatest number of movie releases
 
-import pandas as pd
-import matplotlib.pyplot as plt
-
 df = pd.read_csv('HollywoodMovies.csv')
 
 genre_groups = df.groupby("Genre").groups
 
 genre_groups.keys()
 
+x_pos = [i for i, _ in enumerate(genre_groups.keys())]
+
 plt.bar(genre_groups.keys(), [len(values) for values in genre_groups.values()])
 plt.xlabel("Genre")
 plt.ylabel("Number of movies")
 plt.title("Genre vs Number of Movies")
+
+plt.setp(plt.gca().get_xticklabels(), rotation=90, horizontalalignment='right') # Rotate Axis Labels
+
 plt.grid()
 plt.show()
 
-
 # By seeing plot we can clearly see that Genre which is having most number of movies is -
-#
+# Comedy
