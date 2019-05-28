56cdda370e1b28962452ec4e4af18a24a54f0b0b
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-05-28 20:45:48 +0530

    assignments

--- 25th May Assignments/case study 2/questions.py
+++ 25th May Assignments/case study 2/questions.py
@@ -1,3 +1,6 @@
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,10 +9,11 @@ import numpy as np
 df_cereal = pd.read_csv("cereal.csv")
 print(df_cereal.head())
 
-plt.hist([df_cereal["sugars"], df_cereal["vitamins"]], color=['orange', 'green'])
+plt.hist([df_cereal["sugars"], df_cereal["vitamins"]],
+         color=['orange', 'green'])
 plt.title("Sugars and Vitamins")
 plt.xlabel("Sugars and Vitamins")
-plt.legend(["Sugars"," Vitamins"])
+plt.legend(["Sugars", " Vitamins"])
 plt.show()
 
 
@@ -21,24 +25,24 @@ plt.show()
 # 'G': 'General Mills' ,
 # 'P' :'Post' ,
 # 'A':'American Home Foods Products'
-# Create a bar plot where each manufacturer is on the y axis and the 
+# Create a bar plot where each manufacturer is on the y axis and the
 # height of the bars depict the number of cereals manufactured by them.
 
 dict_mfr = {'N': 'Nabisco',
-'Q': 'Quaker Oats',
-'K': 'Kelloggs',
-'R': 'Raslston Purina',
-'G': 'General Mills' ,
-'P' :'Post' ,
-'A':'American Home Foods Products'}
+            'Q': 'Quaker Oats',
+            'K': 'Kelloggs',
+            'R': 'Raslston Purina',
+            'G': 'General Mills',
+            'P': 'Post',
+            'A': 'American Home Foods Products'}
 
 df_cereal["manufactures"] = [dict_mfr[mfr] for mfr in df_cereal["mfr"]]
 
-grouped_mfr =  df_cereal.groupby(["manufactures"], as_index=False).count()
+grouped_mfr = df_cereal.groupby(["manufactures"], as_index=False).count()
 x = grouped_mfr["manufactures"]
 y = grouped_mfr["mfr"]
 
-plt.bar(x,y)
+plt.bar(x, y)
 
 plt.setp(plt.gca().get_xticklabels(), rotation=90, horizontalalignment='right')
 plt.xlabel("Manufactureres")
@@ -47,19 +51,15 @@ plt.title("number of cereals manufactured by Manufactures")
 plt.show()
 
 
-# 3. Extract the rating as your target variable ‘y’ and all numerical parameters as your predictors ‘x’. 
+# 3. Extract the rating as your target variable ‘y’ and all numerical parameters as your predictors ‘x’.
 # Separate 25% of your data as test set.
 
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
-X = df_cereal.iloc[:,3:15]
+X = df_cereal.iloc[:, 3:15]
 Y = df_cereal["rating"]
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state=10)
-
-
+x_train, x_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.25, random_state=10)
 
 
 # 4. Fit a linear regression module and measure the mean squared error on test dataset.
