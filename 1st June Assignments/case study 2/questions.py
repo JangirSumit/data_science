0b6facb0252c051e4ccd69990ede2d43198339ba
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-06-02 19:00:09 +0530

    assignments

---
+++ 1st June Assignments/case study 2/questions.py
@@ -0,0 +1,9 @@
+# 1.We shall use the same dataset used in previous assignment -digits.
+# Make a 80-20 train/test split.[Hint:Explore datasets module from scikit learn]
+#
+# 2.Using scikit learn perform a LDA on the dataset. Find out the number of components
+# in the projected subspace.[Hint:Refer to discriminant analysis moduleof scikit learn]
+#
+# 3.Transform the dataset and fit a logistic regression and observe the accuracy.
+# Compare it with the previous model based on PCA in terms of accuracy and model complexity.
+# [Hint: Project both the train and test samples to the new subspace]
