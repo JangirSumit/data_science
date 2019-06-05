d7437b8f4c9a53b2a324a5bb689f2cf5d2bc2f8e
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-06-05 10:26:26 +0530

    assignments

--- 2nd June Assignments/case study 3/questions.py
+++ 2nd June Assignments/case study 3/questions.py
@@ -13,11 +13,13 @@ labelencoder = LabelEncoder()
 data["Private"] = labelencoder.fit_transform(data["Private"])
 data.head()
 
+
 # 2.Use LabelEncoder to encode the target variable in to numerical form and split the data such that 20% of the data is set aside fortesting.
 X = data.iloc[:,1:]
 Y = data["Private"]
 
 train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size = 0.30, random_state = 10)
+
 
 # 3.Fit a linear svm from scikit learn and observe the accuracy.[Hint:Use Linear SVC
 model_svm = svm.LinearSVC()
@@ -26,3 +28,26 @@ predicted_values = model_svm.predict(test_x)
 
 print("\nAccuracy Score\n")
 print(metrics.accuracy_score(predicted_values, test_y))
+
+
+# 4.Preprocess the data using StandardScalar and fit the same model again and observe the change in accuracy.
+# [Hint: Refer to scikitlearn’s preprocessing methods]
+# http://benalexkeen.com/feature-scaling-with-scikit-learn/
+from sklearn.preprocessing import StandardScaler
+scaler_df = StandardScaler().fit_transform(X)
+scaler_df = pd.DataFrame(X, columns=X.columns)
+
+X = scaler_df
+Y = data["Private"]
+
+train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size = 0.30, random_state = 10)
+
+model_svm = svm.LinearSVC()
+model_svm.fit(train_x, train_y)
+
+predicted_values = model_svm.predict(test_x)
+metrics.accuracy_score(predicted_values, test_y)
+
+# 5.Use scikit learn’s gridsearch to select the best hyperparameter for a non-linear SVM,identify the model with 
+# best score and its parameters.
+# [Hint: Refer to model_selection module of Scikit learn]
