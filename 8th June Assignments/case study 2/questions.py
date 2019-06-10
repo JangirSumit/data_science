afd0bbd3fb3ccc3d53d90aa08276d723cee28695
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-06-10 14:08:34 +0530

    assignments

--- 8th June Assignments/case study 2/questions.py
+++ 8th June Assignments/case study 2/questions.py
@@ -7,6 +7,9 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
 # Convert PIL image to array
 img = Image.open("dogs.jpeg")
 np_array = np.array(img)
@@ -20,7 +23,7 @@ print("Width : " + str(img.width))
 
 # Reshape the array in 2 dimensions
 flat_array = np_array.ravel()
-new_array = flat_array.reshape(int(len(flat_array)/2), 2)
+new_array = flat_array.reshape(np_array.shape[0] * np_array.shape[1], np_array.shape[2])
 
 
 # Initializing the k means
@@ -37,12 +40,12 @@ unique, counts = np.unique(kmeans.labels_, return_counts=True)
 print(dict(zip(unique, counts)))
 
 
-df = pd.DataFrame(new_array, columns=["X-axis", "Y-axis"])
+df = pd.DataFrame(new_array, columns=["col1", "col2", "col3"])
 df["cluster"] = kmeans.labels_
 print(df.head())
 
 # Plot cluster points
-sns.lmplot('X-axis', 'Y-axis', data=df, hue="cluster",
+sns.lmplot('col1', 'col2', data=df, hue="cluster",
            palette='coolwarm', size=6, aspect=1, fit_reg=False)
 
 
@@ -52,11 +55,19 @@ X_compressed = X_compressed.reshape(
     np_array.shape[0], np_array.shape[1], np_array.shape[2])
 
 
-fig, ax = plt.subplots(1, 2, figsize=(12, 8))
+fig, ax = plt.subplots(1, 2, figsize=(20, 8))
 ax[0].imshow(img)
 ax[0].set_title('Original Image')
 ax[1].imshow(X_compressed)
-ax[1].set_title('Compressed Image with 30 colors')
+ax[1].set_title('Compressed Image with 3 colors')
 for ax in fig.axes:
     ax.axis('off')
 plt.tight_layout()
+
+
+# Find Dominant color 
+centroid_colors = kmeans.cluster_centers_.astype(int)
+colours = [rgb_to_hex(color) for color in centroid_colors]
+
+sns.palplot(colours)
+plt.show()
