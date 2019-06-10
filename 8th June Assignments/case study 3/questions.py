d297f18efa7f26e5a06bc4252dd1015333a8d236
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-06-10 09:45:01 +0530

    assignments

--- 8th June Assignments/case study 3/questions.py
+++ 8th June Assignments/case study 3/questions.py
@@ -7,8 +7,17 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("zoo.csv")
 data.head()
 
-
+# Find Unique class types
 unique_classtypes = np.unique(data["class_type"].values)
 
-agglo = AgglomerativeClustering(n_clusters=16)
-agglo.fit(data.iloc[:, 1:])
+# Initialize Agglomerative Clustering
+agglo = AgglomerativeClustering(n_clusters=4)
+predicted_values = agglo.fit_predict(data.iloc[:, 0:16])
+
+# Accuracy Score
+print("Accuracy Score")
+print(metrics.accuracy_score(predicted_values, data["class_type"].values))
+
+# Mean Square Error value
+print("Mean Squared Error value")
+print(metrics.mean_squared_error(predict, data["class_type"].values))
